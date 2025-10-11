39261fad0d3cfccf6db5cc03d96252b774841149
hugo-snunes <hugo.snunes@a.ucb.br]>
2025-10-11 13:04:30 -0300

    add

--- 1bimestre/Manipulacao_de_arquivos/ex01.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char* argv[]){
-    FILE * fp; // fp é um ponteiro para arquivo
-    printf("%d %x\n", fp, fp); // Mostra o endereço apontado por fp
-    return 0;
-
-}
