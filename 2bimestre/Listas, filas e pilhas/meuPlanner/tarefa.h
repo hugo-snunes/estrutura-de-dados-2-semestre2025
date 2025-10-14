0322871eebcc501422e27813be4d20cbd130f6f8
hugo-snunes <hugo.snunes@a.ucb.br>
2025-10-14 12:57:17 -0300

    add lista

---
+++ 2bimestre/Listas, filas e pilhas/meuPlanner/tarefa.h
@@ -0,0 +1,37 @@
+#ifndef TAREFA_H
+
+#define TAREFA_H
+
+#define TAM_TITULO 96
+#define TAM_NOME 48
+
+#define A_FAZER 0
+#define EM_ANDAMENTO 1
+#define FEITO 2
+
+
+//Definindo a estrutura de um elemento da lista
+typedef struct {
+
+    char Titulo[TAM_TITULO];
+    char Responsavel[TAM_NOME];
+    int Status;
+    float Progresso;
+    int Avaliacao;
+
+} Tarefa;
+
+//Definindo a estrutura da lista de tarefas
+typedef struct {
+    Tarefa * Dados;
+    int Capacidade;
+    int Tamanho;
+} Lista;
+
+Lista * CriarLista(int C);
+void DestruirLista(Lista * L);
+void AdicionarTarefa(Lista * L, Tarefa T);
+
+void GerarHTMLTabela(Lista *Lista, char *CaminhoArquivo);
+
+#endif
